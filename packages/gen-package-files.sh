405604a2f46cdb2ff167424cfbff66c6a48cb513
Jeff Sutherland <jeffdsuth@gmail.com>
2017-05-23 14:19:41 +0700

    Add package restoration via scripts
    Intended as a way to restore everything installed via:
    homebrew, pip, gem, npm, go

---
+++ packages/gen-package-files.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+set -euo pipefail
+
+# Generates package files for homebrew, npm, pip, gem, and go.
+# Intended use for installing packages from fresh install.
+
+# mkdir with timestamp to avoid collisions
+outdir=$(date +%Y_%m_%d_%H%M%S)
+
+_gen_brewfile() {
+  brewfile=Brewfile
+
+  brew bundle dump --file="$outdir/$brewfile"
+}
+
+_gen_pipfile() {
+  pipfile=Pipfile
+
+  pip freeze > "$outdir/$pipfile"
+}
+
+_gen_gemfile() {
+  gemfile=Gemfile
+
+  gem list | cut -d ' ' -f 1 >  "$outdir/$gemfile"
+}
+
+_gen_gofile() {
+  gofile=Gofile
+
+  go list all > "$outdir/$gofile"
+  sed -i '' 's/^github/github/gp;d' "$outdir/$gofile"
+}
+
+_gen_npmfile() {
+  npmfile=Npmfile
+
+  ls "$(npm root -g)" > "$outdir/$npmfile"
+  sed -i '' 's|/||g' "$outdir/$npmfile"
+}
+
+generate_package_files() {
+  mkdir "$outdir" || exit 1
+  echo "Generating package files to $outdir"
+  echo
+  _gen_brewfile
+  _gen_pipfile
+  _gen_gemfile
+  _gen_gofile
+  _gen_npmfile
+  echo
+  echo "Finished generating package files to $outdir"
+}
+
+generate_package_files
+
+exit 0
---
+++ packages/install-package-files.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+# set -euo pipefail
+
+# install_package_files.sh <path>
+#
+# Installs package files for homebrew, npm, pip, gem, and go.
+# Intended use for installing packages from fresh install.
+#
+# NOTE: depends on package files being created from install-package-files.sh
+
+path="$1"
+
+if [ -z "$path" ]; then
+  echo "Usage: install_package_files.sh <path>"; exit 1
+fi
+
+_install_brewfile() {
+  brewfile=Brewfile
+  printf "Installing Homebrew packages:\n"
+
+  brew bundle --file="$path/$brewfile"
+}
+
+_install_pipfile() {
+  pipfile=Pipfile
+  printf "Installing pip packages:\n"
+
+  pip install -r "$path/$pipfile"
+}
+
+_install_gemfile() {
+  gemfile=Gemfile
+  printf "Installing gems:\n"
+
+  while read -r line || [[ -n $line ]]; do
+    sudo gem install "$line"
+  done < "$path/$gemfile"
+}
+
+_install_gofile() {
+  gofile=Gofile
+  printf "Installing go packages:\n"
+
+  while read -r line || [[ -n $line ]]; do
+    go get -v "$line"
+  done < "$path/$gofile"
+}
+
+_install_npmfile() {
+  npmfile=Npmfile
+  printf "Installing npm packages:\n"
+
+  while read -r line || [[ -n $line ]]; do
+    echo "$line"
+    echo
+    # TODO: some fail with yarn -- use npm instead (put in main script?)
+    sudo yarn global add "$line"
+  done < "$path/$npmfile"
+}
+
+install_package_files() {
+  echo "Installing packages from $path"
+  echo
+  # _install_brewfile
+  # _install_pipfile
+  # _install_gemfile
+  # _install_gofile
+  _install_npmfile
+  echo
+  echo "Finished installing packages from $path"
+}
+
+install_package_files
+
+exit 0
