59a2e7187c6dc67831cb3c168e5ddf8256afd5e0
Jeff Sutherland <jeffdsuth@gmail.com>
2023-07-04 16:50:40 +0800

    vim: general readable

--- config/nvim/lua/jdsutherland/general.lua
+++ config/nvim/lua/jdsutherland/general.lua
@@ -1,59 +1,63 @@
-vim.g.mapleader = ','
-vim.g.maplocalleader = '\\'
-vim.opt.smarttab = true
-vim.opt.autowrite = true
-vim.opt.expandtab = true
-vim.opt.shiftwidth = 2
-vim.opt.softtabstop = 2
-vim.opt.shortmess = "aoOtIc"
-vim.opt.pumheight = 20
-vim.opt.tabstop = 2
-vim.opt.mouse = ""
-vim.opt.conceallevel = 2
-vim.opt.smartindent = true
-vim.opt.autoindent = true
-vim.opt.scrolloff = 3
-vim.opt.visualbell = true
-vim.opt.lazyredraw = true
-vim.opt.history = 10000
-vim.opt.updatetime = 250
+local g = vim.g
+local opt = vim.opt
+
+g.mapleader = ','
+g.maplocalleader = '\\'
+g.netrw_liststyle = 3
+
+opt.smarttab = true
+opt.autowrite = true
+opt.expandtab = true
+opt.shiftwidth = 2
+opt.softtabstop = 2
+opt.shortmess = "aoOtIc"
+opt.pumheight = 20
+opt.tabstop = 2
+opt.mouse = ""
+opt.conceallevel = 2
+opt.smartindent = true
+opt.autoindent = true
+opt.scrolloff = 3
+opt.visualbell = true
+opt.lazyredraw = true
+opt.history = 10000
+opt.updatetime = 250
 -- TODO: trying lower amount
-vim.opt.timeoutlen = 300
-vim.opt.statusline = "%t"
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.gdefault = true
-vim.opt.showmatch = true
-vim.opt.wrap = true
-vim.opt.linebreak = true
-vim.opt.wildmode = "longest:full"
-vim.opt.wildignore:append("tmp/**")
-vim.opt.spellfile = "~/.vim/spell/en.utf-8.add"
-vim.opt.thesaurus = "~/.vim/spell/thesaurus/mthesaur.txt"
-vim.opt.undofile = true
-vim.opt.swapfile = false
+opt.timeoutlen = 300
+opt.statusline = "%t"
+opt.ignorecase = true
+opt.smartcase = true
+opt.gdefault = true
+opt.showmatch = true
+opt.wrap = true
+opt.linebreak = true
+opt.wildmode = "longest:full"
+opt.wildignore:append("tmp/**")
+opt.spellfile = "~/.vim/spell/en.utf-8.add"
+opt.thesaurus = "~/.vim/spell/thesaurus/mthesaur.txt"
+opt.undofile = true
+opt.swapfile = false
 
-vim.opt.foldmethod = "expr"
-vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
-vim.opt.foldlevelstart = 99
-vim.opt.foldnestmax = 10
-vim.opt.foldenable = false
-vim.opt.foldlevel = 1
+opt.foldmethod = "expr"
+opt.foldexpr = "nvim_treesitter#foldexpr()"
+opt.foldlevelstart = 99
+opt.foldnestmax = 10
+opt.foldenable = false
+opt.foldlevel = 1
 
-vim.opt.splitbelow = true
-vim.opt.splitright = true
-vim.opt.diffopt:append("vertical")
-vim.opt.termguicolors = true
-vim.opt.listchars = {tab='→\\ ',trail='∙',nbsp='•',eol='¶'}
-vim.opt.grepprg = "rg --smart-case --vimgrep"
-vim.opt.undodir = "~/.config/nvim/undodir"
-vim.opt.showmode = false
-vim.opt.iskeyword:append("-")
-vim.g.netrw_liststyle = 3
-vim.cmd 'cabbrev vsf vert sfind'
-vim.cmd 'colorscheme kanagawa-dragon'
+opt.splitbelow = true
+opt.splitright = true
+opt.diffopt:append("vertical")
+opt.termguicolors = true
+opt.listchars = {tab='→\\ ',trail='∙',nbsp='•',eol='¶'}
+opt.grepprg = "rg --smart-case --vimgrep"
+opt.undodir = "~/.config/nvim/undodir"
+opt.showmode = false
+opt.iskeyword:append("-")
 
 -- TODO: keep?
--- vim.opt.completeopt:remove("preview")
-vim.opt.formatoptions = "qrn1"
-vim.opt.formatoptions:remove("cro")
+-- opt.completeopt:remove("preview")
+opt.formatoptions = "qrn1"
+opt.formatoptions:remove("cro")
+
+vim.cmd 'cabbrev vsf vert sfind'
