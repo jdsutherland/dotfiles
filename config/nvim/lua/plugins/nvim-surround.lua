67f79f91cb8a566223ab8c478e52b7430656ec7c
Jeff Sutherland <jeffdsuth@gmail.com>
2023-07-07 01:07:14 +0800

    vim: add surround

---
+++ config/nvim/lua/plugins/nvim-surround.lua
@@ -0,0 +1,58 @@
+
+return {
+  {
+    "kylechui/nvim-surround",
+    config = function()
+      require("nvim-surround").setup({
+        vim.cmd([[
+        " TODO: working from surround.vim but probably more elegant way to achieve this in nvim-surround
+        " surround Function 
+        nmap ,F ysiwf
+
+        " ,# Surround a word with #{ruby interpolation}
+        nmap ,# ysiw}i#<esc>E
+        vmap ,# c#{<C-R>"}<ESC>
+
+        " ,$ Surround a word with ${js interpolation}
+        nmap ,$ ysiw}i$<esc>E
+        vmap ,$ c${<C-R>"}<ESC>
+
+        " ," Surround a word with "quotes"
+        nmap ," ysiw"
+        vmap ," c"<C-R>""<ESC>
+
+        " ,' Surround a word with 'single quotes'
+        nmap ,' ysiw'
+        vmap ,' c'<C-R>"'<ESC>
+
+        " ,) or ,( Surround a word with (parens)
+        " The difference is in whether a space is put in
+        nmap ,( ysiw(
+        nmap ,) ysiw)
+        vmap ,( c( <C-R>" )<ESC>
+        vmap ,) c(<C-R>")<ESC>
+
+        " ,[ Surround a word with [brackets]
+        nmap ,] ysiw]
+        nmap ,[ ysiw[
+        vmap ,[ c[ <C-R>" ]<ESC>
+        vmap ,] c[<C-R>"]<ESC>
+
+        " ,{ Surround a word with {braces}
+        nmap ,} ysiw}
+        nmap ,{ ysiw{
+        vmap ,} c{ <C-R>" }<ESC>
+        vmap ,{ c{<C-R>"}<ESC>
+
+        nmap ,` ysiw`
+        nmap ,<space> ysiw<space><space>
+
+        " TODO: translate this to nvim-surround
+        " " x Surround jsx
+        " autocmd FileType javascriptreact,typescriptreact,typescript.tsx let b:surround_120 = "<\r />"
+        " autocmd FileType javascriptreact,typescriptreact,typescript.tsx nmap ,X ysiwxe
+        ]])
+      })
+    end
+  }
+}
