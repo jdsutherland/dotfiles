05a402a84aa5c42f00d1c27c40dee7992adb35eb
Jeff Sutherland <jeffdsuth@gmail.com>
2023-07-07 21:20:12 +0800

    vim: add kanagawa readme tweaks

--- config/nvim/lua/plugins/kanagawa.lua
+++ config/nvim/lua/plugins/kanagawa.lua
@@ -4,7 +4,46 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-      require('kanagawa').setup{}
+      require('kanagawa').setup({
+        compilation = true,
+        colors = { theme = { all = { ui = { bg_gutter = "none" } } } },
+        dimInactive = true,
+        theme = "dragon",
+        overrides = function(colors)
+          local theme = colors.theme
+          return {
+            -- https://github.com/rebelot/kanagawa.nvim#borderless-telescope
+            -- TODO: make this darker and make text match fzf
+            TelescopeTitle = { fg = theme.ui.special, bold = true },
+            TelescopePromptNormal = { bg = theme.ui.bg_p1 },
+            TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
+            TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
+            TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
+            TelescopePreviewNormal = { bg = theme.ui.bg_dim },
+            TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
+
+            -- https://github.com/rebelot/kanagawa.nvim#transparent-floating-windows
+            NormalFloat = { bg = "none" },
+            FloatBorder = { bg = "none" },
+            FloatTitle = { bg = "none" },
+            -- Save an hlgroup with dark background and dimmed foreground
+            -- so that you can use it where your still want darker windows.
+            -- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
+            NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+            -- Popular plugins that open floats will link to NormalFloat by default;
+            -- set their background accordingly if you wish to keep them dark and borderless
+            LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+            MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+
+            -- https://github.com/rebelot/kanagawa.nvim#dark-completion-popup-menu
+            Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 },  -- add `blend = vim.o.pumblend` to enable transparency
+            PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+            PmenuSbar = { bg = theme.ui.bg_m1 },
+            PmenuThumb = { bg = theme.ui.bg_p2 },
+          }
+        end
+      })
+
       vim.cmd.colorscheme "kanagawa-dragon"
     end
   }
