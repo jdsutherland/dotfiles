d0c0bb0e0c5f03c315544fa61359f028b1578b0c
Jeff Sutherland <jeffdsuth@gmail.com>
2023-07-08 21:34:16 +0800

    vim: plugin init WIP

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -1,9 +1,56 @@
+local utils = require("jdsutherland.utils")
+local nnoremap = utils.nnoremap
+
 return {
+  { 'ethanholz/nvim-lastplace', config = true },
+  { 'NvChad/nvim-colorizer.lua', config = true },
+  { 'mcauley-penney/tidy.nvim', config = true },
+  { 'gbprod/stay-in-place.nvim', config = true },
+  { "folke/todo-comments.nvim", opts = {} },
   {
-    'ethanholz/nvim-lastplace', config = function() require'nvim-lastplace'.setup() end
+    'Mr-LLLLL/interestingwords.nvim',
+    opts = {
+      colors = { '#9999ff', '#ffb3ff', '#ff7272', '#ffd65c', '#99ffb3', '#a8957e', '#fff6cc' },
+      navigation = true, -- search icon w/ count for hlsearch
+      color_key = "<space>h",
+      cancel_color_key = "<space>H",
+      search_key = nil,
+      cancel_search_key = nil,
+    }
   },
   {
-    'NvChad/nvim-colorizer.lua', config = function() require 'colorizer'.setup() end
+    "chrisgrieser/nvim-various-textobjs",
+    opts = {
+      -- TODO: use il, ae (maybe)
+      useDefaultKeymaps = true
+    },
+  },
+  {
+    "folke/zen-mode.nvim",
+    cmd = "ZenMode",
+    opts = { plugins = {
+      tmux = true,
+      alacritty = { enabled = true, font = "15" },
+    } },
+    keys = { { "<c-w>t", "<cmd>ZenMode<cr>", desc = "Zen Mode" } },
+  },
+  {
+    "folke/which-key.nvim", opts = {},
+    init = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 400
+    end,
+  },
+  {
+    "okuuva/auto-save.nvim",
+    cmd = "ASToggle", -- optional for lazy loading on command
+    event = { "InsertLeave", "TextChanged" },
+    opts = { execution_message = { enabled = false } },
+  },
+  {
+    'windwp/nvim-autopairs', config = function()
+      require("nvim-autopairs").setup({ event = "InsertEnter" })
+    end
   },
   'jghauser/mkdir.nvim',
   -- =========== vimscript plugins ===========
@@ -33,5 +80,10 @@ return {
       'tpope/vim-rhubarb',
       'rbong/vim-flog',
     }
+  },
+  {
+    'AndrewRadev/switch.vim',
+    cmd = "Switch",
+    keys = { { "-", "<cmd>Switch<cr>", desc = "Switch" } },
   },
 }
