3975aff708fb7535ba2e0740a74ad8d19cf79573
Jeff Sutherland <jeffdsuth@gmail.com>
2023-07-09 12:24:03 +0800

    timer calls barbar :BuffOrderByWindowNumber every 10 seconds
    I want barbar buffers to match the visual order. I don't want to call
    the command manually. Using autocmd BufEnter would mostly be
    a waste, so using a 10 second timer.

--- config/nvim/lua/plugins/barbar.lua
+++ config/nvim/lua/plugins/barbar.lua
@@ -7,9 +7,18 @@ return {
     },
     init = function() vim.g.barbar_auto_setup = false end,
     config = function()
-      require('barbar').setup{
-        icons = { button = false }
-      }
+      require('barbar').setup({
+        icons = { button = false },
+        hide = { inactive = true }
+      })
+
+      local timer = vim.loop.new_timer()
+      timer:start(0, 10000, function()
+        vim.defer_fn(function()
+          vim.cmd 'BufferOrderByWindowNumber'
+        end, 0)
+
+      end)
     end
   }
 }
