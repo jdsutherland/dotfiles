62c0219b36a2c1f56e8fecf5f3ba17ea7201c543
Jeff Sutherland <jeffdsuth@gmail.com>
2023-07-22 16:46:04 +0800

    feat: use telescope-fzf-native for generic
    faster & enables useful fzf syntax: https://i.imgur.com/wSqD3pU.png

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -65,7 +65,7 @@ return {
         extensions = {
           fzf = {
             fuzzy = true,                    -- false will only do exact matching
-            override_generic_sorter = false, -- override the generic sorter
+            override_generic_sorter = true, -- override the generic sorter
             override_file_sorter = true,     -- override the file sorter
           },
           vimgrep_arguments = {
