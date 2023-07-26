64cdc3f17813d7b1690e05382362822912656f93
Jeff Sutherland <jeffdsuth@gmail.com>
2023-07-26 13:22:59 +0800

    add desc to harpoon keymaps

--- config/nvim/lua/plugins/harpoon.lua
+++ config/nvim/lua/plugins/harpoon.lua
@@ -5,11 +5,11 @@ return {
     config = function()
       require ("harpoon").setup {}
 
-      vim.keymap.set('n', "<tab>ma", require("harpoon.mark").add_file)
-      vim.keymap.set('n', "<tab>mm", require("harpoon.ui").toggle_quick_menu)
+      vim.keymap.set('n', "<tab>mm", require("harpoon.mark").add_file, { desc = "Harpoon mark"})
+      vim.keymap.set('n', "<tab>m", require("harpoon.ui").toggle_quick_menu, { desc = "Harpoon menu"})
 
       for i = 1, 5 do
-        vim.keymap.set('n', string.format("<space>%s", i), function() require("harpoon.ui").nav_file(i) end)
+        vim.keymap.set('n', string.format("<space>%s", i), function() require("harpoon.ui").nav_file(i) end, { desc = "Harpoon nav"})
       end
     end,
   },
