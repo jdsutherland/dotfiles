17d80fc7fbe30ea5e3ed2bd7e3cb4c592ee0e12a
Jeff Sutherland <jeffdsuth@gmail.com>
2024-11-13 18:59:48 -0500

    update lsp-zero to v4.x

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -1,8 +1,8 @@
 return {
-  { 'folke/neodev.nvim', config = true },
+  { 'folke/lazydev.nvim', config = true }, -- Use lazydev.nvim instead of neodev.nvim
   {
     'VonHeikemen/lsp-zero.nvim',
-    branch = 'v2.x',
+    branch = 'v4.x',
     dependencies = {
       -- LSP Support
       {'neovim/nvim-lspconfig'},             -- Required
@@ -19,40 +19,43 @@ return {
       {'hrsh7th/cmp-nvim-lsp'}, -- Required
       {'L3MON4D3/LuaSnip'},     -- Required
       {'hrsh7th/cmp-path'},
-      {'hrsh7th/cmp-nvim-lsp'},
       {'hrsh7th/cmp-buffer'},
       {'saadparwaiz1/cmp_luasnip'},
       {'rafamadriz/friendly-snippets'},
-      {'ray-x/lsp_signature.nvim', opts = { transparency = 10 }},
-      -- {'hrsh7th/cmp-nvim-lsp-signature-help'},
+      {'ray-x/lsp_signature.nvim', opts = {
+        transparency = 10,
+        hint_enable = true,
+        hint_prefix = {
+          above = "↙ ",  -- when the hint is on the line above the current line
+          current = "← ",  -- when the hint is on the same line
+          below = "↖ "  -- when the hint is on the line below the current line
+        }
+      }},
     },
     config = function()
-      local lsp = require("lsp-zero").preset({})
+      -- Initialize lsp-zero
+      local lsp = require("lsp-zero")
 
-      lsp.ensure_installed({
-      })
+      -- Set up on_attach for keybindings and other settings
+      local lsp_attach = (function(_, bufnr)
+        lsp.default_keymaps({ buffer = bufnr })
 
-      lsp.on_attach(function(_, bufnr)
+        -- Additional custom keybindings
         local function opts_desc(desc)
           return {buffer = bufnr, remap = false, desc = desc}
         end
-
         local _ = vim.lsp
 
-        vim.keymap.set("n", "gd", _.buf.definition, opts_desc('Goto Definition (LSP)'))
-        -- vim.keymap.set("n", "gs", _.buf.signature_help, opts) -- TODO: keep?
-        vim.keymap.set("n", "<c-f>", _.buf.hover, opts_desc('Hover'))
-        vim.keymap.set("i", "<F6>", _.buf.signature_help, opts_desc('Sig Help'))
-        vim.keymap.set("n", "<space>vws", _.buf.workspace_symbol, opts_desc('workspace symbol'))
-        vim.keymap.set("n", "<space>vd", vim.diagnostic.open_float, opts_desc('workspace symbol'))
-        vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts_desc('workspace symbol'))
-        vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts_desc('Goto prev diag'))
-        vim.keymap.set("n", "<space>vca", _.buf.code_action, opts_desc('Goto next diag'))
-        vim.keymap.set("n", "<space>vrr", _.buf.references, opts_desc('qf refs (LSP)'))
-        vim.keymap.set("n", "<space>vi", _.buf.implementation, opts_desc('implementation (LSP)')) -- TODO: keep?
-        vim.keymap.set("n", "<space>vrn", _.buf.rename, opts_desc('Rename (LSP)'))
-        vim.keymap.set('n', '<space>vtd', _.buf.type_definition, opts_desc('Type def (LSP)'))
+        vim.keymap.set("n", "<space>vws", _.buf.workspace_symbol, opts_desc('Workspace Symbol'))
+        vim.keymap.set("n", "<space>vd", vim.diagnostic.open_float, opts_desc('Diagnostics Float'))
+        vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts_desc('Next Diagnostic'))
+        vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts_desc('Previous Diagnostic'))
+        vim.keymap.set("n", "<space>vca", _.buf.code_action, opts_desc('Code Action'))
+        vim.keymap.set("n", "<space>vrr", _.buf.references, opts_desc('References'))
+        vim.keymap.set("n", "<space>vrn", _.buf.rename, opts_desc('Rename'))
+        vim.keymap.set('n', '<space>vtd', _.buf.type_definition, opts_desc('Type Definition'))
 
+        -- Telescope bindings for LSP-related searches
         -- TODO: move to telescope?
         vim.keymap.set('n', '<space>fd', require('telescope.builtin').lsp_document_symbols, opts_desc('Document Symbols'))
         vim.keymap.set('n', '<space>fo', function() require("telescope.builtin").lsp_document_symbols({
@@ -62,11 +65,62 @@ return {
         vim.keymap.set('n', '<space>fw', require('telescope.builtin').lsp_dynamic_workspace_symbols, opts_desc('[W]orkspace Symbols'))
       end)
 
-      -- Fix Undefined global 'vim'
-      require("lspconfig").lua_ls.setup(lsp.nvim_lua_ls())
+      lsp.extend_lspconfig({
+        capabilities = require('cmp_nvim_lsp').default_capabilities(),
+        lsp_attach = lsp_attach,
+        float_border = 'rounded',
+        sign_text = true,
+      })
 
+      -- Mason setup for managing external language servers
+      require('mason').setup({})
+
+      require('mason-lspconfig').setup({
+        ensure_installed = {
+          'bashls',
+          'clangd',
+          'cssls',
+          'gopls',
+          'html',
+          'jsonls',
+          'lua_ls',
+          'marksman',
+          'pyright',
+          'rust_analyzer',
+          'solargraph',
+          'ts_ls',
+          'vimls',
+          'yamlls',
+        },
+        handlers = {
+          function(server_name)
+            require('lspconfig')[server_name].setup({})
+          end
+        }
+      })
+
+      -- Configure the Lua language server (`lua_ls`) for Neovim
+      require("lspconfig").lua_ls.setup({
+        settings = {
+          Lua = {
+            runtime = {
+              version = 'LuaJIT',
+            },
+            diagnostics = {
+              globals = { 'vim' },
+            },
+            workspace = {
+              library = vim.api.nvim_get_runtime_file("", true),
+              checkThirdParty = false,
+            },
+          },
+        },
+      })
+
+      -- Initialize the LSP setup
       lsp.setup()
 
+      -- Autocompletion setup
       local cmp = require("cmp")
       local cmp_action = require('lsp-zero').cmp_action()
 
@@ -74,20 +128,17 @@ return {
       require'luasnip'.filetype_extend("ruby", {"rails"})
 
       cmp.setup({
-
-        -- Make the first item in completion menu always be selected.
+        -- Autocomplete menu behavior
         preselect = 'item',
         completion = {
           completeopt = 'menu,menuone,noinsert'
         },
-
         sources = {
           {name = "path"},
           {name = "nvim_lsp"},
           {name = "nvim_lua"},
           {name = "buffer", keyword_length = 3},
           {name = "luasnip", keyword_length = 2},
-          -- {name = 'nvim_lsp_signature_help'}
         },
         mapping = {
           ["<C-f>"] = function(fallback)
@@ -97,10 +148,12 @@ return {
               fallback()
             end
           end,
-          -- Navigate between snippet placeholder
-          -- TODO: see if this conflicts w/ tmux
+          -- Snippet navigation
           ['<C-j>'] = cmp_action.luasnip_jump_forward(),
           ['<C-k>'] = cmp_action.luasnip_jump_backward(),
+          -- scroll up and down the documentation window
+          ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-d>'] = cmp.mapping.scroll_docs(4),
         }
       })
     end
@@ -124,67 +177,6 @@ return {
       preview_bg_highlight = 'NormalFloat',
       auto_preview = true,
       autofold_depth = 1,
-      -- symbol_blacklist = {
-      --   'File',
-      --   'Module',
-      --   'Namespace',
-      --   'Package',
-      --   'Class',
-      --   'Method',
-      --   'Property',
-      --   'Field',
-      --   'Constructor',
-      --   'Enum',
-      --   'Interface',
-      --   'Function',
-      --   'Variable',
-      --   'Constant',
-      --   'String',
-      --   'Number',
-      --   'Boolean',
-      --   'Array',
-      --   'Object',
-      --   'Key',
-      --   'Null',
-      --   'EnumMember',
-      --   'Struct',
-      --   'Event',
-      --   'Operator',
-      --   'TypeParameter',
-      --   'Component',
-      --   'Fragment',
-      -- },
     }
   },
-  -- { 'https://git.sr.ht/~whynothugo/lsp_lines.nvim', opts = {}, },
-  -- {
-  --   -- LSP Configuration & Plugins
-  --   'neovim/nvim-lspconfig',
-  --   dependencies = {
-  --     -- Automatically install LSPs to stdpath for neovim
-  --     { 'williamboman/mason.nvim', config = true },
-  --     'williamboman/mason-lspconfig.nvim',
-
-  --     -- Useful status updates for LSP
-  --     { 'j-hui/fidget.nvim', tag = 'legacy', opts = {} },
-
-  --     -- Additional lua configuration, makes nvim stuff amazing!
-  --     'folke/neodev.nvim',
-  --   },
-  -- },
-  -- {
-  --   -- Autocompletion
-  --   'hrsh7th/nvim-cmp',
-  --   dependencies = {
-  --     -- Snippet Engine & its associated nvim-cmp source
-  --     'L3MON4D3/LuaSnip',
-  --     'saadparwaiz1/cmp_luasnip',
-
-  --     -- Adds LSP completion capabilities
-  --     'hrsh7th/cmp-nvim-lsp',
-
-  --     -- Adds a number of user-friendly snippets
-  --     'rafamadriz/friendly-snippets',
-  --   },
-  -- }
 }
