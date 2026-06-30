7b1da4a04ed71fba7d60ed98bd8b7c5b280ff989
Jeff Sutherland <jeffdsuth@gmail.com>
2026-06-29 22:48:16 -0700

    vim markdown

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -6,6 +6,13 @@ return {
     opts = true, -- needed even when using default config
   },
   {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+  },
+  {
     -- TODO: try oxi
     'nvim-pack/nvim-spectre',
     opts = {},
