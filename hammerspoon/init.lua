45446bd4ff8fa83362c6fb11569e9fa3b26d978d
Jeff Sutherland <jeffdsuth@gmail.com>
2026-06-29 22:48:54 -0700

    hammerspoon stuff

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,11 +1,14 @@
+-- require ("google_docs")
+-- require ("markbot")
+
 config = {}
 local hyper = {"cmd", "alt", "ctrl", "shift"}
 
-function focusBrave()
-  hs.application.launchOrFocus("Brave Browser Beta")
-end
+-- function focusBrave()
+--   hs.application.launchOrFocus("Brave Browser Beta")
+-- end
 
-hs.hotkey.bind(hyper, "a", focusBrave)
+-- hs.hotkey.bind(hyper, "a", focusBrave)
 
 -- Jump to Whatsapp
 hs.hotkey.bind({"cmd", "shift", "option", "ctrl"}, '`', nil, function()
@@ -55,28 +58,28 @@ hs.hotkey.bind({"cmd", "shift", "option", "ctrl"}, 'm', nil, function()
   ]])
 end)
 
--- Jump to chatgpt
-hs.hotkey.bind({"cmd", "shift", "option", "ctrl"}, 'c', nil, function()
-  hs.osascript.javascript([[
-  (function() {
-    const chrome = Application('Google Chrome');
-    chrome.activate();
-    const win = chrome.windows[0];
-    const tabIndex = win.tabs().findIndex(tab => tab.url().match(/chat.openai.com/));
-    if (tabIndex !== -1) {
-      win.activeTabIndex = (tabIndex + 1);
-      win.index = 1;
-    } else {
-      win.tabs.push(new chrome.Tab());
-      win.activeTab.url = "https://chat.openai.com/chat"
-    }
-  })();
+-- -- Jump to chatgpt
+-- hs.hotkey.bind({"cmd", "shift", "option", "ctrl"}, 'c', nil, function()
+--   hs.osascript.javascript([[
+--   (function() {
+--     const chrome = Application('Google Chrome');
+--     chrome.activate();
+--     const win = chrome.windows[0];
+--     const tabIndex = win.tabs().findIndex(tab => tab.url().match(/chatgpt.com/));
+--     if (tabIndex !== -1) {
+--       win.activeTabIndex = (tabIndex + 1);
+--       win.index = 1;
+--     } else {
+--       win.tabs.push(new chrome.Tab());
+--       win.activeTab.url = "https:///chatgpt.com/"
+--     }
+--   })();
 
-  ]])
-end)
+--   ]])
+-- end)
 
 -- Jump to gdocs spreadsheet
-hs.hotkey.bind({"cmd", "shift", "option", "ctrl"}, ';', nil, function()
+hs.hotkey.bind({"cmd", "shift", "option", "ctrl"}, 's', nil, function()
   hs.osascript.javascript([[
   (function() {
     let chrome = Application('Google Chrome');
@@ -99,79 +102,194 @@ hs.hotkey.bind({"cmd", "shift", "option", "ctrl"}, ';', nil, function()
   ]])
 end)
 
--- Jump to sportsbook daily thread
-hs.hotkey.bind({"cmd", "shift", "option", "ctrl"}, 'n', nil, function()
-  hs.osascript.javascript([[
-  (function() {
-    const openTodaysDailyThread = "[...document.querySelectorAll('.title')].find(e => e.textContent.includes('Sportsbook/Promos/Bonuses Daily')).children[1].click()"
+-- -- Jump to gemini
+-- hs.hotkey.bind({"cmd", "shift", "option", "ctrl"}, 'z', nil, function()
+--   hs.osascript.javascript([[
+--   (function() {
+--     let chrome = Application('Google Chrome');
+--     chrome.activate();
+--     for (win of chrome.windows()) {
+--       let tabIndex =
+--         win.tabs().findIndex(tab => tab.url().match(/gemini.google.com/));
+--       if (tabIndex != -1) {
+--         win.activeTabIndex = (tabIndex + 1);
+--         win.index = 1;
+--         let app = Application.currentApplication();
+--         app.includeStandardAdditions = true;
+--         delay(0.01)
+--         app.doShellScript('open -a "Google Chrome"');
+--         return;
+--       }
+--     }
+--   })();
+--   ]])
+-- end)
 
-    let chrome = Application('Google Chrome');
-    chrome.activate();
-    for (win of chrome.windows()) {
-      let tabIndex =
-        win.tabs().findIndex(tab => tab.url().includes("sportsbookpromosbonuses_daily"));
-      if (tabIndex != -1) {
-        win.activeTabIndex = (tabIndex + 1);
-        win.index = 1;
-        win.activeTab.execute({ javascript: "window.location.reload()" });
-        return;
-      } else {
-        win.tabs.push(new chrome.Tab());
-        win.activeTab.url = "https://old.reddit.com/r/sportsbook"
-        delay(2)
-        win.activeTab.execute({ javascript: openTodaysDailyThread })
-        return;
-      }
-    }
-  })();
-  ]])
-end)
+-- -- Jump to browse odds
+-- hs.hotkey.bind({"cmd", "shift", "option", "ctrl"}, 'x', nil, function()
+--   hs.osascript.javascript([[
+--   (function() {
+--     let chrome = Application('Google Chrome');
+--     chrome.activate();
+--     for (win of chrome.windows()) {
+--       let tabIndex =
+--         win.tabs().findIndex(tab => tab.url().match(/darkhorseodds.com\/browse/));
+--       if (tabIndex != -1) {
+--         win.activeTabIndex = (tabIndex + 1);
+--         win.index = 1;
+--         let app = Application.currentApplication();
+--         app.includeStandardAdditions = true;
+--         delay(0.01)
+--         app.doShellScript('open -a "Google Chrome"');
+--         return;
+--       }
+--     }
+--   })();
+--   ]])
+-- end)
 
--- Jump to dh arbs
-hs.hotkey.bind({"cmd", "shift", "option", "ctrl"}, 'z', nil, function()
-  hs.osascript.javascript([[
-  (function() {
-    let chrome = Application('Google Chrome');
-    chrome.activate();
-    for (win of chrome.windows()) {
-      let tabIndex =
-        win.tabs().findIndex(tab => tab.url().match(/darkhorseodds.com\/arbitrage/));
-      if (tabIndex != -1) {
-        win.activeTabIndex = (tabIndex + 1);
-        win.index = 1;
-        let app = Application.currentApplication();
-        app.includeStandardAdditions = true;
-        delay(0.01)
-        app.doShellScript('open -a "Google Chrome"');
-        return;
-      }
-    }
-  })();
-  ]])
-end)
+-- function chumbaClearAndReaddDontPass()
+--     local script = [[
+--         tell application "Google Chrome" to activate
+--         on clickAt(x, y)
+--             do shell script "/opt/homebrew/bin/cliclick c:" & x & "," & y
+--         end clickAt
 
--- Jump to browse odds
-hs.hotkey.bind({"cmd", "shift", "option", "ctrl"}, 'x', nil, function()
-  hs.osascript.javascript([[
-  (function() {
-    let chrome = Application('Google Chrome');
-    chrome.activate();
-    for (win of chrome.windows()) {
-      let tabIndex =
-        win.tabs().findIndex(tab => tab.url().match(/darkhorseodds.com\/browse/));
-      if (tabIndex != -1) {
-        win.activeTabIndex = (tabIndex + 1);
-        win.index = 1;
-        let app = Application.currentApplication();
-        app.includeStandardAdditions = true;
-        delay(0.01)
-        app.doShellScript('open -a "Google Chrome"');
-        return;
-      }
-    }
-  })();
-  ]])
-end)
+--         clickAt(1239, 1234) -- click x
+--         delay 0.2
+
+--         clickAt(800, 844) -- confirm clear
+
+--         -- readd dont pass chip
+--         repeat 4 times
+--           delay 0.3
+--           clickAt(900, 930)
+--         end repeat
+
+--         -- -- -- try every dont come number
+--         repeat 3 times
+--           delay 0.2
+--           clickAt(550, 356)
+--           clickAt(686, 356)
+--           clickAt(812, 356)
+--           clickAt(960, 356)
+--           clickAt(1110, 356)
+--           clickAt(1260, 356)
+--         end repeat
+--     ]]
+--     hs.osascript.applescript(script)
+-- end
+-- hs.hotkey.bind({"cmd", "shift", "option", "ctrl"}, ";", chumbaClearAndReaddDontPass)
+
+-- ASSUMPTIONS: messages.google 2nd tab, LL last tab and chrome is focused on LL homepage (logged in)
+function luckylandBuy()
+  local script = [[
+  tell application "Google Chrome" to activate
+  on clickAt(x, y)
+    do shell script "/opt/homebrew/bin/cliclick c:" & x & "," & y
+  end clickAt
+
+  clickAt(600, 192) -- click buy
+  delay 2
+
+  clickAt(1062, 849) -- click "$19.99"
+  delay 2
+  clickAt(1000, 535) -- click discover card
+  delay 1
+  tell application "System Events" to tell process "Google Chrome" to keystroke "528"
+  delay 1
+
+  -- tell application "Google Chrome" to activate
+  -- delay 1
+  -- tell application "System Events"
+  --   tell process "Google Chrome"
+  --       set frontmost to true
+  --       clickAt(562, 746)
+  --       delay 1
+  --       tell application "System Events"
+  --           keystroke "5"
+  --           delay 0.2
+  --           keystroke "2"
+  --           delay 0.2
+  --           keystroke "8"
+  --       end tell
+  --   end tell
+  -- end tell
+  -- delay .2
+
+  clickAt(1065, 875) -- click "Pay $19.99"
+  delay 8 -- wait for discover to load
+
+  clickAt(878, 605) -- click "continue" btn; sends text
+  delay 4
+
+  -- go to texts (assumes 2nd tab)
+  tell application "Google Chrome"
+    tell window 1
+      set active tab index to 2
+    end tell
+  end tell
+  delay 1
+
+  clickAt(223, 294) -- click newest text (discover)
+  delay 1
+
+  do shell script "screencapture -R610,1130,530,75 /tmp/sms_code.png"
+  delay 2
+
+  do shell script "/opt/homebrew/bin/tesseract /tmp/sms_code.png /tmp/ocr_output"
+  delay 1
+  set ocrText to do shell script "cat /tmp/ocr_output.txt"
+  delay 1
+
+  -- Use AppleScript to extract the 6-digit code
+  set code to do shell script "echo " & quoted form of ocrText & " | grep -o '\\b[0-9]\\{6\\}\\b' | head -n 1"
+  delay 1
+
+  -- Switch to the last tab in Chrome (assumes LL)
+  tell application "Google Chrome" to activate
+  delay 1 -- Ensures Chrome is active
+  tell application "Google Chrome"
+    set lastTabIndex to count of tabs in window 1
+    tell window 1
+      set active tab index to lastTabIndex
+    end tell
+  end tell
+  delay 1
+
+  clickAt(905, 501) -- click the discover code field
+  delay 1
+
+  -- Paste the OCR result as keystrokes
+  tell application "System Events" to keystroke ocrText
+
+  ]]
+  hs.osascript.applescript(script)
+end
+-- hs.hotkey.bind({"cmd", "shift", "option", "ctrl"}, ";", luckylandBuy)
+
+-- function chumbaClear()
+--     local script = [[
+--         tell application "Google Chrome" to activate
+--         on clickAt(x, y)
+--             do shell script "/opt/homebrew/bin/cliclick c:" & x & "," & y
+--         end clickAt
+
+--         clickAt(1239, 1234) -- click x
+--         delay 0.1
+
+--         clickAt(800, 844) -- confirm clear
+
+--         -- readd dont pass chip
+--         repeat 4 times
+--           delay 0.1
+--           clickAt(900, 930)
+--         end repeat
+--     ]]
+--     hs.osascript.applescript(script)
+-- end
+-- hs.hotkey.bind({"cmd", "shift", "option", "ctrl"}, "'", chumbaClear)
+
 
 -- -- Jump to fanduel
 -- hs.hotkey.bind({"cmd", "shift", "option", "ctrl"}, 's', nil, function()
@@ -192,9 +310,9 @@ end)
 --       win.activeTab.url = "https://sportsbook.fanduel.com"
 --       delay(2)
 --     }
---     const js_click_login_btn = "document.evaluate('/html/body/div[1]/div/div/div/div[1]/header/div[2]/div[3]/a[1]/div/div', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.click()"
+--     // const js_click_login_btn = "document.evaluate('/html/body/div[1]/div/div/div/div[1]/header/div[2]/div[3]/a[1]/div/div', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.click()"
 
---     win.activeTab.execute({ javascript: js_click_login_btn })
+--     // win.activeTab.execute({ javascript: js_click_login_btn })
 --   })();
 --   ]])
 -- end)
