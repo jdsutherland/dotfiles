2148a7baa6818837fd0ce9e309f4f1a51e9c6a44
Jeff Sutherland <jeffdsuth@gmail.com>
2026-07-04 11:20:54 -0700

    refactor: convert init.vim to init.lua

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,5 +1,3 @@
-" {{{ lua config
-lua << EOF
 require('jdsutherland.general')
 require('jdsutherland.keymaps')
 require('jdsutherland.autocmds')
@@ -12,18 +10,15 @@ require('jdsutherland.autocmds')
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
-  "git",
-  "clone",
-  "--filter=blob:none",
-  "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
     lazypath,
   })
 end
 vim.opt.rtp:prepend(lazypath)
 require("lazy").setup("plugins", {
-  change_detection = { enabled = false }
+  change_detection = { enabled = false },
 })
-
-EOF
-" }}}
