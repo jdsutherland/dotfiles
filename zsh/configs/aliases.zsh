6d60aac67f547fc395fd9c9d09c24d3d8b6dcba4
Jeff Sutherland <jeffdsuth@gmail.com>
2026-07-04 20:36:07 -0700

    fix alias

--- zsh/configs/aliases.zsh
+++ zsh/configs/aliases.zsh
@@ -67,7 +67,7 @@ if [[ -o interactive ]]; then
   alias mi='e ~/.dotfiles/config/mpv/input.conf'
   alias sshe='e ~/.ssh/config'
   alias te='e ~/.dotfiles/tmux.conf'
-  alias ve='e ~/.dotfiles/config/nvim/init.vim'
+  alias ve='e ~/.dotfiles/config/nvim/init.lua'
   alias ze='e ~/.dotfiles/zshrc'
   alias zs='source ~/.zshrc'
 
