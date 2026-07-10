67d0835f75603397f96ff474c37b9af802894e80
Jeff Sutherland <jeffdsuth@gmail.com>
2026-07-09 19:43:39 -0700

    add more snacks

--- config/nvim/lua/plugins/snacks.lua
+++ config/nvim/lua/plugins/snacks.lua
@@ -4,6 +4,24 @@ return {
   lazy = false,
   ---@type snacks.Config
   opts = {
+    -- picker/indent/explorer/zen/scope/terminal are deliberately left off
+    -- since telescope/mini.indentscope/mini.files/zen-mode.nvim already
+    -- cover those
     image = { enabled = true },
+    quickfile = { enabled = true },
+    notifier = { enabled = true },
+    dashboard = { enabled = true },
+    input = { enabled = true },
+    bigfile = { enabled = true },
+    scratch = { enabled = true },
   },
+  config = function(_, opts)
+    require("snacks").setup(opts)
+    vim.api.nvim_create_user_command("Scratch", function()
+      Snacks.scratch()
+    end, { desc = "Toggle Scratch Buffer" })
+    vim.api.nvim_create_user_command("ScratchSelect", function()
+      Snacks.scratch.select()
+    end, { desc = "Select Scratch Buffer" })
+  end,
 }
